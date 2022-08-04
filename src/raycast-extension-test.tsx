d1266473a2042d73c5ef77c48b21142ac2600a5b
shige-ywyw <shige46732@yahoo.co.jp>
2022-08-05 00:54:08 +0900

    yahoo news import

--- src/raycast-extension-test.tsx
+++ src/raycast-extension-test.tsx
@@ -1,8 +1,9 @@
 // エラーあり！！！
-// https://menthas.com/  rss取得
+// news  rss取得
 import { Action, ActionPanel, List, open } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { useFetch, Response } from "@raycast/utils";
+import { useFetch, Response, isLoading } from "@raycast/utils";
+import { XMLParser } from "fast-xml-parser";
 import Parser from "rss-parser";
 
 const parser_hatena = new Parser();
@@ -17,6 +18,14 @@ interface State {
 
 export default function Command() {
   const [state, setState] = useState<State>({});
+  const { isLoading, data } = useFetch(
+    "https://news.yahoo.co.jp/rss/topics/top-picks.xml"
+  );
+  const options = {
+    ignoreAttributes: true,
+  };
+  const parser_yahoo = new XMLParser(options);
+  const json = parser_yahoo.parse(data).rss;
 
   useEffect(() => {
     async function fetchStories() {
@@ -61,86 +70,21 @@ export default function Command() {
           </ActionPanel>
         }
       />
+      <List.Item
+        title="yahoo news"
+        actions={
+          <ActionPanel title="menu">
+            <ActionPanel.Submenu title="yahoo news">
+              {(json["channel"]["item"] || []).map((item) => (
+                <Action
+                  title={item["title"]}
+                  onAction={() => open(item["link"])}
+                />
+              ))}
+            </ActionPanel.Submenu>
+          </ActionPanel>
+        }
+      />
     </List>
   );
 }
-
-//hatena boookmark
-/*
-import { Action, ActionPanel, List, Detail } from "@raycast/api";
-import { useEffect, useState } from "react";
-import { useFetch, Response } from "@raycast/utils";
-import Parser from "rss-parser";
-
-const parser_hatena = new Parser();
-const options = {
-  ignoreAttributes: true,
-};
-interface State {
-  items?: Parser.Item[];
-  error?: Error;
-}
-
-export default function Command() {
-  const [state, setState] = useState<State>({});
-
-  useEffect(() => {
-    async function fetchStories() {
-      let feed = await parser_hatena.parseURL(
-        "https://b.hatena.ne.jp/hotentry/it.rss"
-      );
-      setState({ items: feed.items });
-    }
-    fetchStories();
-  }, []);
-  return (
-    <List>
-      {(state.items || []).map((item) => (
-        <List.Item
-          title={item["title"]}
-          actions={
-            <ActionPanel>
-              <Action.OpenInBrowser url={item["link"]} />
-            </ActionPanel>
-          }
-        />
-      ))}
-    </List>
-  );
-}
-*/
-
-// yahoo news topics get
-/*
-import { Action, ActionPanel, List, Detail } from "@raycast/api";
-import { useEffect, useState } from "react";
-import { useFetch, Response } from "@raycast/utils";
-import { XMLParser } from "fast-xml-parser";
-
-
-export default function Command() {
-  const { isLoading, data } = useFetch(
-    "https://news.yahoo.co.jp/rss/topics/top-picks.xml"
-  );
-  const options = {
-    ignoreAttributes: true,
-  };
-  const parser_yahoo = new XMLParser(options);
-
-  const json = parser_yahoo.parse(data).rss;
-  return (
-    <List>
-      {(json["channel"]["item"] || []).map((item) => (
-        <List.Item
-          title={item["title"]}
-          actions={
-            <ActionPanel>
-              <Action.OpenInBrowser url={item["link"]} />
-            </ActionPanel>
-          }
-        />
-      ))}
-    </List>
-  );
-}
-*/
